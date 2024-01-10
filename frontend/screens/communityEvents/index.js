18859facfabbe8f72f97b4bb71684e387b5bea4d
MoeKhaweja <mohammadkhawaja1999@gmail.com>
2024-01-10 23:16:36 +0200

    [frontend]: fixed style issues add event

--- frontend/screens/communityEvents/index.js
+++ frontend/screens/communityEvents/index.js
@@ -127,7 +127,7 @@ const ViewCommunityEvents = () => {
       <FAB
         style={styles.fab}
         icon='plus'
-        onPress={() => navigation.navigate("CompleteProfilePage")}
+        onPress={() => navigation.navigate("CommunityAddEvents")}
       />
     </View>
   );
--- frontend/screens/communityEventsAdd/index.js
+++ frontend/screens/communityEventsAdd/index.js
@@ -26,6 +26,14 @@ import { getCommunityEvents } from "../../store/user";
 const CommunityAddEvents = () => {
   const [image, setImage] = useState(null);
   const [base64, setBase64] = useState(null);
+  const [eventDetails, setEventDetails] = useState({
+    title: "",
+    description: "",
+    schedule: "",
+    location: "",
+    duration: 0,
+    image: "", // URL to the event image
+  });
 
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
@@ -51,7 +59,7 @@ const CommunityAddEvents = () => {
     console.log("Creating event:", eventDetails);
     // Implement logic to create an event in your database here
     // Then update the events list using setEvents([...events, newEvent])
-    setModalVisible(false); // Close the modal after creating an event
+
     // Reset event details after creating the event
     setEventDetails({
       title: "",
@@ -65,107 +73,98 @@ const CommunityAddEvents = () => {
 
   return (
     <View style={styles.container}>
-      <ScrollView style={styles.modalContainer}>
-        <TextInput
-          label='Title'
-          value={eventDetails.title}
-          onChangeText={(text) =>
-            setEventDetails({ ...eventDetails, title: text })
-          }
+      <TextInput
+        label='Title'
+        value={eventDetails.title}
+        onChangeText={(text) =>
+          setEventDetails({ ...eventDetails, title: text })
+        }
+        style={styles.input}
+      />
+      <TextInput
+        label='Description'
+        value={eventDetails.description}
+        onChangeText={(text) =>
+          setEventDetails({ ...eventDetails, description: text })
+        }
+        style={styles.input}
+      />
+      <TextInput
+        label='Schedule'
+        value={eventDetails.schedule}
+        onChangeText={(text) =>
+          setEventDetails({ ...eventDetails, schedule: text })
+        }
+        style={styles.input}
+      />
+      <TextInput
+        label='Location'
+        value={eventDetails.location}
+        onChangeText={(text) =>
+          setEventDetails({ ...eventDetails, location: text })
+        }
+        style={styles.input}
+      />
+      {image ? (
+        <View>
+          <TouchableOpacity onPress={removeImage}>
+            <Icon name='times-circle' size={20} color='red' />
+          </TouchableOpacity>
+          <Image
+            resizeMode='cover'
+            height={150}
+            style={{ marginBottom: 10, borderRadius: 15 }}
+            source={{ uri: image }}
+          />
+        </View>
+      ) : (
+        <Button
+          mode='contained'
+          icon={() => <Icon name='camera' size={20} color='white' />}
+          onPress={pickImage}
           style={styles.input}
-        />
-        <TextInput
-          label='Description'
-          value={eventDetails.description}
-          onChangeText={(text) =>
-            setEventDetails({ ...eventDetails, description: text })
-          }
-          style={styles.input}
-        />
-        <TextInput
-          label='Schedule'
-          value={eventDetails.schedule}
-          onChangeText={(text) =>
-            setEventDetails({ ...eventDetails, schedule: text })
-          }
-          style={styles.input}
-        />
-        <TextInput
-          label='Location'
-          value={eventDetails.location}
-          onChangeText={(text) =>
-            setEventDetails({ ...eventDetails, location: text })
-          }
-          style={styles.input}
-        />
-        {image ? (
-          <View>
-            <TouchableOpacity onPress={removeImage}>
-              <Icon name='times-circle' size={20} color='red' />
-            </TouchableOpacity>
-            <Image
-              resizeMode='cover'
-              height={150}
-              style={{ marginBottom: 10, borderRadius: 15 }}
-              source={{ uri: image }}
-            />
-          </View>
-        ) : (
-          <Button
-            mode='contained'
-            icon={() => <Icon name='camera' size={20} color='white' />}
-            onPress={pickImage}
-          >
-            Pick Image
-          </Button>
-        )}
-        <TextInput
-          label='Image URL'
-          value={eventDetails.image}
-          onChangeText={(text) =>
-            setEventDetails({ ...eventDetails, image: text })
-          }
-          style={styles.input}
-        />
-        <TextInput
-          label='Duration (in minutes)'
-          value={eventDetails.duration.toString()}
-          onChangeText={(text) =>
-            setEventDetails({
-              ...eventDetails,
-              duration: parseFloat(text) || 0,
-            })
-          }
-          keyboardType='numeric'
-          style={styles.input}
-        />
-        <Button mode='contained' onPress={handleCreateEvent}>
-          Create Event
+        >
+          Pick Image
         </Button>
-      </ScrollView>
+      )}
+      <TextInput
+        label='Image URL'
+        value={eventDetails.image}
+        onChangeText={(text) =>
+          setEventDetails({ ...eventDetails, image: text })
+        }
+        style={styles.input}
+      />
+      <TextInput
+        label='Duration (in minutes)'
+        value={eventDetails.duration.toString()}
+        onChangeText={(text) =>
+          setEventDetails({
+            ...eventDetails,
+            duration: parseFloat(text) || 0,
+          })
+        }
+        keyboardType='numeric'
+        style={styles.input}
+      />
+      <Button mode='contained' onPress={handleCreateEvent}>
+        Create Event
+      </Button>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    padding: 20,
+    alignContent: "center",
+    justifyContent: "flex-start",
     flex: 1,
   },
   card: {
     margin: 10,
   },
-  fab: {
-    position: "absolute",
-    margin: 16,
-    right: 0,
-    bottom: 0,
-  },
-  modalContainer: {
-    backgroundColor: "white",
-    padding: 20,
-    margin: 40,
-    borderRadius: 10,
-  },
+
   input: {
     marginBottom: 10,
   },
