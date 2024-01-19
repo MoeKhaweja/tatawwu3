a2539ea571a1d18c7a89be872047b9fccb280c71
MoeKhaweja <mohammadkhawaja1999@gmail.com>
2024-01-19 03:32:22 +0200

    [frontend]: updated community event details design

--- frontend/screens/communityEventDetails/index.js
+++ frontend/screens/communityEventDetails/index.js
@@ -1,16 +1,9 @@
-import { useFocusEffect, useNavigation } from "@react-navigation/native";
-import { useCallback, useEffect, useState } from "react";
-import { View, StyleSheet, ScrollView, Text, Image } from "react-native";
-import {
-  Button,
-  Card,
-  IconButton,
-  Paragraph,
-  TextInput,
-  Title,
-} from "react-native-paper";
+import React, { useCallback, useState } from "react";
+import { View, StyleSheet, Text, Image, FlatList } from "react-native";
+import { Button, Card, IconButton, Chip } from "react-native-paper";
 import { useDispatch } from "react-redux";
 import { accept, reject } from "../../store/user";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
 
 const CommunityEventDetails = ({ route }) => {
   const { event } = route.params;
@@ -23,7 +16,9 @@ const CommunityEventDetails = ({ route }) => {
     useCallback(() => {
       try {
         console.log(event);
-      } catch {}
+      } catch (error) {
+        console.error(error);
+      }
     }, [])
   );
 
@@ -32,7 +27,9 @@ const CommunityEventDetails = ({ route }) => {
     console.log("Accepting applicant:", applicantId);
     try {
       dispatch(accept({ applicantId: applicantId, eventId: event._id }));
-    } catch {}
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   // Function to handle denying an applicant
@@ -40,82 +37,112 @@ const CommunityEventDetails = ({ route }) => {
     console.log("Denying applicant:", applicantId);
     try {
       dispatch(reject({ applicantId: applicantId, eventId: event._id }));
-    } catch {}
+    } catch (error) {
+      console.error(error);
+    }
   };
 
+  // Data for FlatList
+  const eventDetailsData = [
+    { key: "Title", value: eventDetails.title },
+    { key: "Description", value: eventDetails.description },
+    { key: "Location", value: eventDetails.location },
+    { key: "Date", value: eventDetails.schedule.date },
+    {
+      key: "Time",
+      value: `${eventDetails.schedule.startTime} - ${eventDetails.schedule.endTime}`,
+    },
+    { key: "Duration", value: `${eventDetails.duration} minutes` },
+    { key: "Targeted Skills", value: eventDetails.targetedSkills },
+  ];
+
+  const renderEventDetail = ({ item }) => (
+    <>
+      <View style={styles.detailItem}>
+        <Text style={styles.detailKey}>{item.key}:</Text>
+        {<Text style={styles.detailValue}>{item.value}</Text>}
+      </View>
+    </>
+  );
+
   return (
-    <ScrollView style={styles.container}>
-      <View style={styles.section}>
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          <Text style={styles.heading}>Event Details</Text>
-          <IconButton
-            icon='pencil'
-            size={20}
-            onPress={() =>
-              navigation.navigate("CommunityEditEvent", {
-                event: eventDetails,
-              })
-            }
-          ></IconButton>
-        </View>
+    <FlatList
+      style={styles.container}
+      data={[{ key: "eventDetails", value: eventDetails }, ...event.applicants]}
+      renderItem={({ item }) => {
+        if (item.key === "eventDetails") {
+          return (
+            <Card style={styles.card}>
+              <View style={styles.eventDetailsHeader}>
+                <Text style={styles.heading}>Event Details</Text>
+                <IconButton
+                  icon='pencil'
+                  size={20}
+                  onPress={() =>
+                    navigation.navigate("CommunityEditEvent", {
+                      event: eventDetails,
+                    })
+                  }
+                />
+              </View>
+              <FlatList
+                data={eventDetailsData}
+                renderItem={renderEventDetail}
+                keyExtractor={(item) => item.key}
+              />
+              <View style={styles.chipsContainer}>
+                {eventDetails.targetedSkills.map((skill, index) => (
+                  <Chip key={index} style={styles.chip}>
+                    {skill}
+                  </Chip>
+                ))}
+              </View>
 
-        <Card style={styles.applicant}>
-          <Text>- {eventDetails.title}</Text>
-          <Text>- {eventDetails.description}</Text>
-          <Text>- {eventDetails.location}</Text>
-          <Text>- {eventDetails.schedule}</Text>
-          <Text>- {eventDetails.duration} minutes</Text>
-          <Image
-            resizeMode='cover'
-            style={{ width: "100%", height: 150 }}
-            src={`http://192.168.1.5:8000/images/${eventDetails.img}`}
-          ></Image>
-        </Card>
-      </View>
-
-      {/* Display list of applicants */}
-      <View style={styles.section}>
-        <Text style={styles.heading}>Applicants</Text>
-        {event.applicants?.map((applicant) => (
-          <Card
-            key={applicant._id}
-            style={styles.applicant}
-            onPress={() => {
-              console.log(applicant);
-              navigation.navigate("ProfilePage", { user: applicant.user });
-            }}
-          >
-            <Card.Content
-              style={{ flexDirection: "row", justifyContent: "space-between" }}
-            >
-              <Text>
-                {applicant.user.firstName + " " + applicant.user.lastName}
-              </Text>
-              <Text>Status: {applicant.status}</Text>
-            </Card.Content>
-            <Card.Actions>
-              {(applicant.status === "applied" ||
-                applicant.status === "pending") && (
-                <View style={styles.buttonsContainer}>
-                  <Button onPress={() => handleAcceptApplicant(applicant._id)}>
-                    Accept
-                  </Button>
-                  <Button onPress={() => handleDenyApplicant(applicant._id)}>
-                    Deny
-                  </Button>
-                </View>
+              {eventDetails.img && (
+                <Image
+                  resizeMode='cover'
+                  style={{ width: "100%", height: 150, marginTop: 10 }}
+                  source={{
+                    uri: `http://192.168.1.5:8000/images/${eventDetails.img}`,
+                  }}
+                />
               )}
-            </Card.Actions>
-          </Card>
-        ))}
-      </View>
-    </ScrollView>
+            </Card>
+          );
+        } else {
+          return (
+            <Card
+              key={item._id}
+              style={styles.applicantCard}
+              onPress={() => {
+                console.log(item);
+                navigation.navigate("ProfilePage", { user: item.user });
+              }}
+            >
+              <Card.Content style={styles.applicantContent}>
+                <Text style={styles.applicantName}>
+                  {item.user.firstName + " " + item.user.lastName}
+                </Text>
+                <Text>Status: {item.status}</Text>
+              </Card.Content>
+              <Card.Actions>
+                {(item.status === "applied" || item.status === "pending") && (
+                  <View style={styles.buttonsContainer}>
+                    <Button onPress={() => handleAcceptApplicant(item._id)}>
+                      Accept
+                    </Button>
+                    <Button onPress={() => handleDenyApplicant(item._id)}>
+                      Deny
+                    </Button>
+                  </View>
+                )}
+              </Card.Actions>
+            </Card>
+          );
+        }
+      }}
+      keyExtractor={(item) => (item.key ? "eventDetails" : item._id)}
+    />
   );
 };
 
@@ -124,30 +151,54 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
   },
-  section: {
-    marginBottom: 20,
+  detailItem: {
+    flexDirection: "row",
+    marginBottom: 5,
+  },
+  detailKey: {
+    fontWeight: "bold",
+    marginRight: 5,
+  },
+  detailValue: {
+    flex: 1,
+  },
+  card: {
+    borderColor: "#ccc",
+    padding: 10,
+    marginBottom: 10,
+  },
+  applicantCard: {
+    borderColor: "#ccc",
+    marginBottom: 10,
+  },
+  applicantContent: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  applicantName: {
+    fontWeight: "bold",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  chipsContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    marginTop: 5,
+  },
+  chip: {
+    marginRight: 5,
+    marginBottom: 5,
   },
   heading: {
     fontSize: 18,
     fontWeight: "bold",
     marginBottom: 10,
   },
-  input: {
-    marginBottom: 10,
-  },
-  applicant: {
-    borderColor: "#ccc",
-    padding: 10,
-    marginBottom: 10,
-    marginHorizontal: 2,
-  },
-  applicantActions: {
+  eventDetailsHeader: {
     flexDirection: "row",
     justifyContent: "space-between",
-    marginTop: 10,
-  },
-  buttonsContainer: {
-    flexDirection: "row",
+    alignItems: "center",
   },
 });
 
