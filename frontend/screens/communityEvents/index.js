6467a896575cded43bd270b2b5c26efa7321c57d
MoeKhaweja <mohammadkhawaja1999@gmail.com>
2024-01-19 03:12:33 +0200

    [frontend]: updated community events page

--- frontend/screens/communityEvents/index.js
+++ frontend/screens/communityEvents/index.js
@@ -1,7 +1,6 @@
-import { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { View, StyleSheet, ScrollView } from "react-native";
-import { Card, Title, Paragraph, FAB } from "react-native-paper";
-
+import { Card, Title, Paragraph, FAB, Chip } from "react-native-paper";
 import { useDispatch, useSelector } from "react-redux";
 import { getCommunityEvents } from "../../store/user";
 import { useFocusEffect, useNavigation } from "@react-navigation/native";
@@ -11,34 +10,95 @@ const ViewCommunityEvents = () => {
   const dispatch = useDispatch();
   const communityEvents = useSelector((state) => state.user.communityEvents);
 
+  // Filter state
+  const [filter, setFilter] = useState("all");
+
   useFocusEffect(
     useCallback(() => {
       try {
         dispatch(getCommunityEvents());
-      } catch {}
+      } catch (error) {
+        console.error(error);
+      }
     }, [])
   );
 
+  // Function to filter events based on date
+  const filterEvents = (eventDate) => {
+    const currentDate = new Date();
+    const eventDateTime = new Date(eventDate);
+
+    if (eventDateTime < currentDate) {
+      return "past";
+    } else if (eventDateTime >= currentDate) {
+      return "upcoming";
+    }
+  };
+
+  // Function to render filter chips
+  const renderFilterChips = () => {
+    return (
+      <View style={styles.filterChipsContainer}>
+        <Chip
+          mode='outlined'
+          onPress={() => setFilter("all")}
+          selected={filter === "all"}
+          style={styles.filterChip}
+        >
+          All Events
+        </Chip>
+        <Chip
+          mode='outlined'
+          onPress={() => setFilter("past")}
+          selected={filter === "past"}
+          style={styles.filterChip}
+        >
+          Past Events
+        </Chip>
+        <Chip
+          mode='outlined'
+          onPress={() => setFilter("upcoming")}
+          selected={filter === "upcoming"}
+          style={styles.filterChip}
+        >
+          Upcoming Events
+        </Chip>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
+      {renderFilterChips()}
       <ScrollView>
-        {communityEvents.events?.map((event, index) => (
-          <Card
-            key={index}
-            style={styles.card}
-            onPress={() =>
-              navigation.navigate("CommunityEventDetails", { event: event })
-            }
-          >
-            {/* Include necessary event details in the card */}
-            <Card.Cover src={`http://192.168.1.5:8000/images/${event.img}`} />
-            <Card.Content>
-              <Title>{event.title}</Title>
-              <Paragraph>{event.description}</Paragraph>
-              {/* Add more details as needed */}
-            </Card.Content>
-          </Card>
-        ))}
+        {communityEvents.events?.map((event, index) => {
+          const { title, description, schedule } = event;
+          const { date, startTime, endTime } = schedule;
+          const filterResult = filterEvents(date);
+
+          // Skip events based on the selected filter
+          if (filter !== "all" && filterResult !== filter) {
+            return null;
+          }
+
+          return (
+            <Card
+              key={index}
+              style={styles.card}
+              onPress={() =>
+                navigation.navigate("CommunityEventDetails", { event: event })
+              }
+            >
+              <Card.Content>
+                <Title>{title}</Title>
+                <Paragraph>{description}</Paragraph>
+                <Paragraph>
+                  Date: {date}, Time: {startTime} - {endTime}
+                </Paragraph>
+              </Card.Content>
+            </Card>
+          );
+        })}
       </ScrollView>
       <FAB
         style={styles.fab}
@@ -62,14 +122,13 @@ const styles = StyleSheet.create({
     right: 0,
     bottom: 0,
   },
-  modalContainer: {
-    backgroundColor: "white",
-    padding: 20,
-    margin: 40,
-    borderRadius: 10,
+  filterChipsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginVertical: 10,
   },
-  input: {
-    marginBottom: 10,
+  filterChip: {
+    marginHorizontal: 5,
   },
 });
 
