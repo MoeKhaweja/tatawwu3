f512222b79fc88a4f011fba9760fc137f95c2d6e
MoeKhaweja <mohammadkhawaja1999@gmail.com>
2024-01-20 22:50:26 +0200

    [backend]: connected middleware for admin

--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -8,7 +8,7 @@ const {
 const { login } = require("../controllers/auth.controllers");
 const { isAdmin } = require("../middlewares/admin.middleware");
 
-router.get("/", getAllUsers);
+router.get("/", isAdmin, getAllUsers);
 router.post("/login", login);
 router.post("/update", updateUserById);
 router.post("/delete", deleteUserById);
