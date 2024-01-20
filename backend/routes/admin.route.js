99656520850f6df7ccbde759c5ee111fed00af0e
MoeKhaweja <mohammadkhawaja1999@gmail.com>
2024-01-20 23:04:30 +0200

    [backend]: updated admin routes

--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -7,11 +7,10 @@ const {
 } = require("../controllers/admin.controller");
 const { login } = require("../controllers/auth.controllers");
 const { isAdmin } = require("../middlewares/admin.middleware");
-const { authMiddleware } = require("../middlewares/auth.middleware");
 
-router.get("/", authMiddleware, isAdmin, getAllUsers);
+router.get("/", isAdmin, getAllUsers);
 router.post("/login", isAdmin, login);
-router.post("/update", authMiddleware, isAdmin, updateUserById);
-router.post("/delete", authMiddleware, isAdmin, deleteUserById);
+router.post("/update", isAdmin, updateUserById);
+router.post("/delete", isAdmin, deleteUserById);
 
 module.exports = router;
