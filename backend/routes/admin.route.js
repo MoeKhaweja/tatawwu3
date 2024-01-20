a8d47b4ad58494fc36eec2cdb8867805e4e7335f
MoeKhaweja <mohammadkhawaja1999@gmail.com>
2024-01-20 22:48:00 +0200

    [backend]: updated admin routes

--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -5,8 +5,10 @@ const {
   updateUserById,
   deleteUserById,
 } = require("../controllers/admin.controller");
+const { login } = require("../controllers/auth.controllers");
 
 router.get("/", getAllUsers);
+router.post("/login", login);
 router.post("/update", updateUserById);
 router.post("/delete", deleteUserById);
 
