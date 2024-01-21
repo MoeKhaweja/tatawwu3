816894042f728599f168f84f938cd9ed9c3a7e98
MoeKhaweja <mohammadkhawaja1999@gmail.com>
2024-01-21 16:07:57 +0200

    [frontend]: updated profile page skills style

--- frontend/screens/profile/index.js
+++ frontend/screens/profile/index.js
@@ -10,9 +10,11 @@ import {
   List,
   Divider,
   Button,
+  Chip,
 } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useSelector } from "react-redux";
+import theme from "../../theme";
 
 const ProfilePage = ({ route, userInfo }) => {
   const current = useSelector((state) => state.user.user.user);
@@ -39,6 +41,20 @@ const ProfilePage = ({ route, userInfo }) => {
               <Paragraph>{user.bio}</Paragraph>
 
               <Divider style={styles.divider} />
+              <View style={styles.chipsContainer}>
+                {user.skills?.map((skill, index) => (
+                  <Chip
+                    key={index}
+                    textStyle={{ color: "white" }}
+                    style={{
+                      margin: 2,
+                      backgroundColor: theme.colors.tertiary,
+                    }}
+                  >
+                    {skill}
+                  </Chip>
+                ))}
+              </View>
 
               <List.Section>
                 <List.Subheader>Skills</List.Subheader>
@@ -88,6 +104,10 @@ const ProfilePage = ({ route, userInfo }) => {
 const styles = StyleSheet.create({
   container: {
     padding: 16,
+  },
+  chipsContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
   },
   topSection: {
     flexDirection: "row",
