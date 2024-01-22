6b4974264b6c6ce98147106785cc3b39be9002fb
MoeKhaweja <mohammadkhawaja1999@gmail.com>
2024-01-22 20:33:17 +0200

    [frontend]: added searchVolunteers global variable

--- frontend/store/user.js
+++ frontend/store/user.js
@@ -38,6 +38,7 @@ const initialState = {
   communityEvents: [],
   volunteerEvents: [],
   searchEvents: [],
+  searchVolunteers: [],
   volunteerMatchingEvents: [],
   eventsApplicationStatus: [],
   eventApplicants: [],
