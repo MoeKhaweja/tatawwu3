c5f1c66928566af7430d4e6b99fb8742a6759efe
MoeKhaweja <mohammadkhawaja1999@gmail.com>
2024-01-24 19:18:21 +0200

    [backend]: updated admin middleware comments

--- backend/middlewares/admin.middleware.js
+++ backend/middlewares/admin.middleware.js
@@ -1,5 +1,13 @@
 const User = require("../models/user.model");
 
+/**
+ * Checks if a user is an admin.
+ * @async
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ * @returns {void} Calls the next middleware function if the user is an admin, otherwise sends a response with an error.
+ */
 const isAdmin = async (req, res, next) => {
   if (req.body.email) {
     try {
